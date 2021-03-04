d0d5ca990f67e8ab6bc2df5d21bb046a49982476
mmy <maxamuudit@gmail.com>
2021-03-04 08:46:04 +0300

    'ecommerce-app'

---
+++ app/Models/Brand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    protected $table = 'brands';
+
+    public $fillable = [
+      'name',
+      'slug',
+      'logo'
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
---
+++ app/Models/Category.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'name', 'slug', 'description', 'parent_id', 'featured', 'menu', 'image'
+    ];
+
+    protected $casts = [
+        'parent_id' => 'integer',
+        'featured' => 'boolean',
+        'menu' => 'boolean'
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->belongsToMany(Category::class, 'parent_id');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'product_categories', 'category_id', 'product_id');
+    }
+}
